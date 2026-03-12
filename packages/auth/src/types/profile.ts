cb6ab19984511019ebaed8c8cfcbb2c0e4cab38d
alessiotegoni <alessiotegoni4@gmail.com>
2026-03-12 15:31:50 +0100

    fix: profile type

--- packages/auth/src/types/profile.ts
+++ packages/auth/src/types/profile.ts
@@ -1,6 +1,6 @@
 import type { User as SupabaseUser } from "@supabase/supabase-js";
 
-import type { User as DBUser } from "@beeto/db/schema";
+import type { Profile as DBUser } from "@beeto/db/schema";
 
 export type User = SupabaseUser & { db: DBUser };
 
